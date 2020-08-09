a58f2468fed23e0563e1f3fb6aacbbdd6a5fa859
tahash <talhamuhamnad7@gmail.com>
2020-08-09 20:50:32 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,40 @@
+function main(){
+    var score = 0;
+   var right  =  document.getElementById("q-1");
+    var question1 =  document.getElementById("q-2");
+    var question2 = document.getElementById("q-3");
+    var question3 = document.getElementById("q-4");
+    if(right.checked == true ){
+score ++
+alert("q1 answer right")
+    }
+    else{
+        alert("q1 answer wrong")
+    }
+
+
+   var right2 = document.getElementById("qqq") ; 
+   var question1 = document.getElementById("q-1") ;
+   var question2 = document.getElementById("q-2") 
+   var question3= document.getElementById("q-4") 
+   if(right2.checked == true){
+       score ++
+       alert("q2 is right")
+   }
+    else{
+        alert("q2 is wrong")
+    }
+
+
+
+
+
+    var right3 = document.getElementById("q-1");
+    if(right3.checked == true){
+        alert("q3 is right")
+
+    }
+else{
+    alert("q3 is wrong")
+}
+}  
